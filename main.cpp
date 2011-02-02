7673704fe1309325c22b201b416425f35df1b767
Alasdair <deek0146@gmail.com>
2011-02-02 23:37:34 +0000

    Fixed, simple bison grammer Is able to parse enough of bison to understand its own source code, semantics time. Lexer is pretty much complete, and the program crashes on exit, probably a string fuck up somewhere

--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
 int yyparse();
+#include <vector>
+#include <iostream>
 #include <cstdio>
+using namespace std;
 extern FILE* yyin;
 int main(int argv, char* argc[])
 {
   yyin = fopen("Parser.y","r");
   yyparse();
+  cout << "Success" << endl;
   return 0;
 }
-#include <vector>
-#include <iostream>
-using namespace std;
 extern int line, character, lastToken;
-struct Token
-    {
-	Token(int t,char* c){token = t;match = c;}
-	int token;
-	string match;
-    };
-extern vector<Token> tokens;
 extern char* yytext;
 void yyerror(char* s)
 {
@@ -25,9 +19,5 @@ void yyerror(char* s)
        << "line: " << line << endl
        << "character: " << character << endl
        << "matched string: " << yytext << (int)yytext[0] << endl
-       << "last tokens: " << endl;
-  for (int i = 1; i < 7; i++)
-    {
-      cout << tokens[tokens.size()-i].token << endl << tokens[tokens.size()-i].match << endl;
-    }
+       << "last token: " << lastToken << endl;
 }
